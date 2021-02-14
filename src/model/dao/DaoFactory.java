273260eae73336be8b0f4ac17b1f24f7ed4b9d09
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-14 11:59:46 -0300

    DepartmentDao implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory { // Essa classe terá operações estáticas para instânciar os Dao's
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
